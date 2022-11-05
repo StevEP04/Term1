0dd605eb174523711fb5c8483d0cf48515ffe220
StevEP04 <steven.espinoza@csedge.org>
2022-11-05 15:46:23 -0400

    droping beery

--- Catchem/berry.py
+++ Catchem/berry.py
@@ -4,22 +4,21 @@ from time import sleep
 
 sense = SenseHat()
 class Berry:
-    def  __init__(self, sense, color, speed, position, value):
+    def  __init__(self, color, speed, value):
         self.color = color
         self.speed = speed
-        self.position = position 
-        self.sense = sense
-        # random.randint(0,7)
+        self.posx = random.randint(0,7)
+        self.posy = 0
         self.value = value
     def drop (self):
-        self.display()
-        if self.position>55:
-            self.position += 8
+        if self.posy<7:
+            self.posy += 1
             sleep(self.speed)
-            # self.display()
+            self.display()
     def display(self):
-        x = self.position%8
-        y = self.position/8
-        self.sense.set_pixel = (x,y-1, (255, 255, 255))
-        # sense.set_pixel = (x,y, self.color)
-
+        x = self.posx
+        y = self.posy
+        if self.posy > 0:
+            sense.set_pixel(x,y-1, (0, 0, 0))
+        sense.set_pixel(x,y, (255,255,255))
+        
--- Catchem/game.py
+++ Catchem/game.py
@@ -5,8 +5,7 @@ from player import Player
 from berry import Berry
 sense = SenseHat()
 
-white = (255, 255, 255)
-berry_post = random.randint(0,7)
+color = [255, 255, 255]
 class Game():
     def __init__(self):
         self.score = 0
@@ -14,12 +13,14 @@ class Game():
         self.speed = 1
         self.berries = []
         self.player = Player(sense, 56,63)
-        self.berry = Berry(sense, white, 1, berry_post, 0)
+        self.berry = Berry(color, 1, 0)
     def play(self):
         self.start()
-        self.berry.drop()
-        self.player.display(0)
+        self.berry.display()
         while not self.game_over:
+            self.player.display(0)
+            self.berry.display()
+            self.berry.drop()
             for event in sense.stick.get_events():
                 if event.action == "pressed" and event.direction == "left":
                     self.player.move_left()
